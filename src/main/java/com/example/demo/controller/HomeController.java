ab29cd501e857dc3440a900098bdc4af966f0530
zhans <zhansultanken@gmail.com>
2025-06-19 10:51:19 +0500

    Исправил снова

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
-    @GetMapping
-    @RequestMapping("/home")
+    @GetMapping("/home")
     public String getHomePage(){
         return "home_page";
     }
