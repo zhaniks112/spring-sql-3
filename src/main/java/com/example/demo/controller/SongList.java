09a1204c1a9d3951cec53b0f09d67b75b5cf1c4b
zhans <zhansultanken@gmail.com>
2025-06-19 12:52:20 +0500

    Changed

--- src/main/java/com/example/demo/controller/SongList.java
+++ src/main/java/com/example/demo/controller/SongList.java
@@ -1,14 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/songs")
+public class SongList {
     @GetMapping
-    public String getHomePage(){
-        return "home_page";
+    public String getSongPage(Model model, String login) {
+        model.addAttribute("userLogin", login);
+        return "song_page";
     }
 }
